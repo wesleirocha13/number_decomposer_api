c9928f5cc768584a93df531172b227418a5642c5
Weslei Souza <weslei.souza@acuttis.com.br>
2022-06-02 18:49:16 -0300

    Criado os middlewares

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const compression = require('compression');
+const { errorHandler, notFoundHandler } = require('./middlewares');
 
 const routes = require('./routes');
 
@@ -14,5 +15,8 @@ app.use(bodyParser.json({ extended: true, limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(routes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
